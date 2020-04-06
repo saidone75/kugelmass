29edfa02978dec85904b93d3b9749f024c0f72b8
saidone <saidone@saidone.org>
2020-04-06 08:11:56 +0200

    redirect for acme challenge

--- src/clj/kugelmass/routes.clj
+++ src/clj/kugelmass/routes.clj
@@ -2,11 +2,13 @@
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response redirect]]))
 
 (defn home-routes [endpoint]
   (routes
    (resources "/")
+   (GET "/.well-known/acme-challenge/:key" [key]
+        (redirect (str "http://acme.saidone.org/.well-known/acme-challenge/" key)))
    (ANY "*" _
         (-> "public/index.html"
             io/resource
