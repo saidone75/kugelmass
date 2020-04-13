9c66ba41b0da4bcee29f7e8f6517d59434d99dc2
saidone <saidone@saidone.org>
2020-04-13 17:15:35 +0200

    life-utils purely functional

--- src/cljs/kugelmass/pages/life/life.cljs
+++ src/cljs/kugelmass/pages/life/life.cljs
@@ -1,13 +1,15 @@
 (ns kugelmass.pages.life.life
   (:require [kugelmass.pages.life.life-utils :as life-utils]))
 
-(def window-width  (.-innerWidth js/window))
-(def window-height  (.-innerHeight js/window))
+(defonce window-width  (.-innerWidth js/window))
+(defonce window-height  (.-innerHeight js/window))
 
-(def blocksize 20)
+(defonce blocksize 20)
 
-(def w (quot (* .80 window-width) blocksize))
-(def h (quot (* .70 window-height) blocksize))
+(defonce board (atom {}))
+
+(swap! board assoc :w (quot (* .80 window-width) blocksize))
+(swap! board assoc :h (quot (* .70 window-height) blocksize))
 
 (def uniqkey (atom 0))
 (defn- gen-key []
@@ -19,28 +21,31 @@
                 :y y
                 :fill color}])
 
-(defn- draw-board [width height blocksize]
-  [:div.board
-   [:svg.board {:width width :height height}
-    (loop [board @life-utils/board blocks nil x 0 y 0 i 0]
-      (if (empty? board) blocks
-          (recur (rest board)
-                 (conj blocks ^{:key (gen-key)} [block x y
-                                                 (if (first board)
-                                                   "#566a12"
-                                                   "#f0f0d0")])
-                 (if (= 0 (mod (inc i) life-utils/width))
-                   0
-                   (+ blocksize x))
-                 (if (= 0 (mod (inc i) life-utils/width))
-                   (+ blocksize y)
-                   y)
-                 (inc i))))]])
+(defn- draw-board [width height]
+  (let [w (:w @board)]
+    [:div.board
+     [:svg.board {:width width :height height}
+      (loop [board (:board @board) blocks nil x 0 y 0 i 0]
+        (if (empty? board) blocks
+            (recur (rest board)
+                   (conj blocks ^{:key (gen-key)} [block x y
+                                                   (if (first board)
+                                                     "#566a12"
+                                                     "#f0f0d0")])
+                   (if (= 0 (mod (inc i) w))
+                     0
+                     (+ blocksize x))
+                   (if (= 0 (mod (inc i) w))
+                     (+ blocksize y)
+                     y)
+                   (inc i))))]]))
 
 (defn render-board []
-  (life-utils/init-game w h)
-  (draw-board (* blocksize w) (* blocksize h) blocksize))
+  (let [{w :w h :h} @board]
+    (swap! board assoc :board (life-utils/init-game @board))
+    (draw-board (* blocksize w) (* blocksize h))))
 
 (defn update-board []
-  (life-utils/compute-next-gen)
-  (draw-board (* blocksize w) (* blocksize h) blocksize))
+  (let [{w :w h :h} @board]
+    (swap! board assoc :board (life-utils/compute-next-gen @board))
+    (draw-board (* blocksize w) (* blocksize h))))
--- src/cljs/kugelmass/pages/life/life_utils.cljs
+++ src/cljs/kugelmass/pages/life/life_utils.cljs
@@ -6,74 +6,71 @@
     true
     false))
 
-;; board status
-(defonce board (atom nil))
-
 ;; init board
-(defn init-game [w h]
-  (def width w)
-  (def height h)
-  (reset! board (vec (doall (take (* width height) (repeatedly random-bool))))))
+(defn init-game [board]
+  (let [{w :w h :h} board]
+    (vec (doall (take (* w h) (repeatedly random-bool))))))
 
 ;; calculate vector index from coords
-(defn- compute-index [x y]
-  (+ x (* y width)))
+(defn- compute-index [x y board]
+  (+ x (* y (:w board))))
 
 ;; calculate grid coords from vector index
-(defn- compute-coords [n]
-  {:x (mod n width) :y (quot n width)})
+(defn- compute-coords [n board]
+  {:x (mod n (:w board)) :y (quot n (:w board))})
 
 ;; check if a cell is alive
-(defn- is-alive [n]
-  (true? (nth @board n)))
+(defn- is-alive [n board]
+  (true? (nth (:board board) n)))
 
 ;; get neighbours of a cell
-(defn- neighbours [n]
-  (let [coords (compute-coords n)
+(defn- neighbours [n board]
+  (let [coords (compute-coords n board)
+        w (:w board)
+        h (:h board)
         ;; inc x and y with wrapping logic
         incx #(cond
-                (= %1 (dec width)) 0
+                (= %1 (dec w)) 0
                 :else (inc %1))
         decx #(cond
-                (= %1 0) (dec width)
+                (= %1 0) (dec w)
                 :else (dec %1))
         incy #(cond
-                (= %1 (dec height)) 0
+                (= %1 (dec h)) 0
                 :else (inc %1))
         decy #(cond
-                (= %1 0) (dec height)
+                (= %1 0) (dec h)
                 :else (dec %1))]
-    [(compute-index (decx (:x coords)) (decy (:y coords)))
-     (compute-index (:x coords) (decy (:y coords)))
-     (compute-index (incx (:x coords)) (decy (:y coords)))
-     (compute-index (decx (:x coords)) (:y coords))
-     (compute-index (incx (:x coords)) (:y coords))
-     (compute-index (decx (:x coords)) (incy (:y coords)))
-     (compute-index (:x coords) (incy (:y coords)))
-     (compute-index (incx (:x coords)) (incy (:y coords)))]))
+    [(compute-index (decx (:x coords)) (decy (:y coords)) board)
+     (compute-index (:x coords) (decy (:y coords)) board)
+     (compute-index (incx (:x coords)) (decy (:y coords)) board)
+     (compute-index (decx (:x coords)) (:y coords) board)
+     (compute-index (incx (:x coords)) (:y coords) board)
+     (compute-index (decx (:x coords)) (incy (:y coords)) board)
+     (compute-index (:x coords) (incy (:y coords)) board)
+     (compute-index (incx (:x coords)) (incy (:y coords)) board)]))
 
 ;; get alive neighbours count
-(defn- count-alive-neighbours [n]
-  (let [neighbours (neighbours n)]
+(defn- count-alive-neighbours [n board]
+  (let [neighbours (neighbours n board)]
     (reduce
-     #(if (is-alive %2)
+     #(if (is-alive %2 board)
         (inc %1)
         %1)
      0
      neighbours)))
 
 ;; compute next generation of board
-(defn compute-next-gen []
-  (reset! board
-          (reduce
-           #(let [alive-neighbours (count-alive-neighbours (count %1))]
-              (if %2
-                (cond
-                  (< alive-neighbours 2) (conj %1 false)
-                  (and (> alive-neighbours 1) (< alive-neighbours 4)) (conj %1 true)
-                  (> alive-neighbours 3) (conj %1 false))
-                (if (= alive-neighbours 3)
-                  (conj %1 true)
-                  (conj %1 false))))
-           []
-           @board)))
+(defn compute-next-gen [board]
+  (reduce
+   #(let [alive-neighbours (count-alive-neighbours (count %1) board)]
+      (if %2
+        (cond
+          (< alive-neighbours 2) (conj %1 false)
+          (and (> alive-neighbours 1) (< alive-neighbours 4)) (conj %1 true)
+          (> alive-neighbours 3) (conj %1 false))
+        (if (= alive-neighbours 3)
+          (conj %1 true)
+          (conj %1 false))))
+   []
+   (:board board)))
