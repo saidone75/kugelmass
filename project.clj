56ae32cff3e824f509989c9279f2ef7fee6f073c
saidone <saidone@saidone.org>
2020-04-19 10:47:30 +0200

    another bunch of dependencies updated

--- project.clj
+++ project.clj
@@ -4,22 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597" :scope "provided"]
-                 [com.cognitect/transit-clj "0.8.313"]
+                 [com.cognitect/transit-clj "1.0.324"]
                  [ring "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
                  [bk/ring-gzip "0.3.0"]
                  [radicalzephyr/ring.middleware.logger "0.6.0"]
                  [clj-logging-config "1.9.12"]
                  [environ "1.1.0"]
-                 [com.stuartsierra/component "0.3.2"]
-                 [org.danielsz/system "0.4.1"]
-                 [org.clojure/tools.namespace "0.2.11"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [org.danielsz/system "0.4.5"]
+                 [org.clojure/tools.namespace "1.0.0"]
                  [clj-time "0.15.2"]
                  [compojure "1.6.1"]
                  [reagent "0.10.0"]
-                 [secretary "1.2.3"]]
+                 [clj-commons/secretary "1.2.4"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-environ "1.1.0"]]
