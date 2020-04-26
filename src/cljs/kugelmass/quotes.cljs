e6df367f6f006b590fddee33fef75795e96ef94d
saidone <saidone@saidone.org>
2020-04-26 12:24:27 +0200

    quotes reworked

--- src/cljs/kugelmass/quotes.cljs
+++ src/cljs/kugelmass/quotes.cljs
@@ -1,73 +1,69 @@
 (ns kugelmass.quotes)
 
-(def quotes '("Frank Zappa"
-              "Politics is the entertainment branch of industry."
-              "Frank Zappa"
-              "There are three things that smell of fish. One of them is fish. The other two are growing on you!"
-              "Frank Zappa"
-              "There is more stupidity than hydrogen in the universe, and it has a longer shelf life."
-              "Frank Zappa"
-              "Tobacco is my favorite vegetable."
-              "Steve Jobs"
-              "It's more fun to be a pirate than to join the Navy."
-              "Robert Sewell"
-              "If Java had true garbage collection, most programs would delete themselves upon execution."
-              "Linus Torvalds"
-              "The memory management on the PowerPC can be used to frighten small children."
-              "Bjarne Stroustrup"
-              "In C++ it's harder to shoot yourself in the foot, but when you do, you blow off your whole leg."
-              "Bjarne Stroustrup"
-              "There are only two kinds of programming languages: those people always bitch about and those nobody uses."
-              "Leonard Brandwein"
-              "Beware of programmers who carry screwdrivers."
-              "Bernie Cosell"
-              "Too many people, too few sheep."
-              "Bob Gray"
-              "Writing in C or C++ is like running a chain saw with all the safety guards removed."
-              "Jeff Sickel"
-              "Deleted code is debugged code."
-              "Timothy Leary"
-              "You're only as young as the last time you changed your mind."
-              "Groucho Marx"
-              "Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read."
-              "Groucho Marx"
-              "I find television very educating. Every time somebody turns on the set, I go into the other room and read a book."
-              "Pablo Picasso"
-              "Computers are useless. They can only give you answers."
-              "Ken Thompson"
-              "One of my most productive days was throwing away 1,000 lines of code."
-              "Brian Kernighan"
-              "Controlling complexity is the essence of computer programming."
-              "Richard Hamming"
-              "Typing is no substitute for thinking."
-              "L Peter Deutsch"
-              "To iterate is human, to recurse divine."
-              "Robert C. Martin"
-              "Truth can only be found in one place: the code."
-              "Alan J. Perlis"
-              "A language that doesn't affect the way you think about programming, is not worth knowing."
-              "Alan J. Perlis"
-              "If you have a procedure with 10 parameters, you probably missed some."
-              "Alan J. Perlis"
-              "LISP programmers know the value of everything and the cost of nothing."
-              "Alan J. Perlis"
-              "Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it."
-              "Donald Knuth"
-              "Beware of bugs in the above code; I have only proved it correct, not tried it."
-              "Edsger W. Dijkstra"
-              "Simplicity is prerequisite for reliability."
-              "Voltaire"
-              "Quand il s’agit d’argent, tout le monde est de la même religion."
-              "Voltaire"
-              "Le mieux est l'ennemi du bien."
+(def quotes '("Leonard Brandwein"
+              #{"Beware of programmers who carry screwdrivers."}
               "Peter Clemenza"
-              "Leave the gun, take the cannoli."))
+              #{"Leave the gun, take the cannoli."}
+              "Bernie Cosell"
+              #{"Too many people, too few sheep."}
+              "L Peter Deutsch"
+              #{"To iterate is human, to recurse divine."}
+              "Edsger W. Dijkstra"
+              #{"Simplicity is prerequisite for reliability."}
+              "Bob Gray"
+              #{"Writing in C or C++ is like running a chain saw with all the safety guards removed."}
+              "Richard Hamming"
+              #{"Typing is no substitute for thinking."}
+              "Douglas Hofstadter"
+              #{"Irrationality is the square root of all evil."
+                "Hofstadter's Law: It always takes longer than you expect, even when you take into account Hofstadter's Law."}
+              "Steve Jobs"
+              #{"It's more fun to be a pirate than to join the Navy."}
+              "Brian Kernighan"
+              #{"Controlling complexity is the essence of computer programming."}
+              "Donald Knuth"
+              #{"Beware of bugs in the above code; I have only proved it correct, not tried it."}
+              "Timothy Leary"
+              #{"You're only as young as the last time you changed your mind."}
+              "Robert C. Martin"
+              #{"Truth can only be found in one place: the code."}
+              "Groucho Marx"
+              #{"Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read."
+                "I find television very educating. Every time somebody turns on the set, I go into the other room and read a book."}
+              "Alan Perlis"
+              #{"A language that doesn't affect the way you think about programming, is not worth knowing."
+                "If you have a procedure with 10 parameters, you probably missed some."
+                "LISP programmers know the value of everything and the cost of nothing."
+                "Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it."}
+              "Pablo Picasso"
+              #{"Computers are useless. They can only give you answers."}
+              "Robert Sewell"
+              #{"If Java had true garbage collection, most programs would delete themselves upon execution."}
+              "Jeff Sickel"
+              #{"Deleted code is debugged code."}
+              "Bjarne Stroustrup"
+              #{"In C++ it's harder to shoot yourself in the foot, but when you do, you blow off your whole leg."
+                "There are only two kinds of programming languages: those people always bitch about and those nobody uses."}
+              "Ken Thompson"
+              #{"One of my most productive days was throwing away 1,000 lines of code."}
+              "Linus Torvalds"
+              #{"The memory management on the PowerPC can be used to frighten small children."}
+              "Voltaire"
+              #{"Quand il s’agit d’argent, tout le monde est de la même religion."
+                "Le mieux est l'ennemi du bien."}
+              "Frank Zappa"
+              #{"Politics is the entertainment branch of industry."
+                "There are three things that smell of fish. One of them is fish. The other two are growing on you!"
+                "There is more stupidity than hydrogen in the universe, and it has a longer shelf life."
+                "Tobacco is my favorite vegetable."}))
 
-(defn- make-quotes [quotes]
-  (reduce
-   #(conj %1 {:author (first %2) :quote (last %2)})
-   []
-   (partition 2 quotes)))
+(defn- make-quotes [quoter]
+  (let [quotes (partition 2 quotes)]
+    (reduce
+     #(concat %1 (for [quote (last %2)]
+                   {:author (first %2) :quote quote}))
+     '() 
+     quotes)))
 
 (def memo-make-quotes (memoize make-quotes))
 
