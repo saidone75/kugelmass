8818835691bc42feb31b9fa3014cf842b01ba3bc
saidone <saidone@saidone.org>
2020-05-03 17:23:45 +0200

    bump ClojureScript version to 1.10.753

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.741" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.753" :scope "provided"]
                  [com.cognitect/transit-clj "1.0.324"]
                  [ring "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
