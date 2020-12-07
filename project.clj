72925b99ed0a4510a169b46f9dac9c877657b331
saidone <saidone@saidone.org>
2020-12-07 10:13:30 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://github.com/saidone75/kugelmass/blob/master/LICENSE"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.764" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [com.cognitect/transit-clj "1.0.324"]
                  [ring "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
@@ -18,7 +18,7 @@
                  [org.clojure/tools.namespace "1.0.0"]
                  [clj-time "0.15.2"]
                  [compojure "1.6.1"]
-                 [reagent "1.0.0-alpha2"]
+                 [reagent "1.0.0-rc1"]
                  [clj-commons/secretary "1.2.4"]]
 
   :plugins [[lein-cljsbuild "1.1.8"]
