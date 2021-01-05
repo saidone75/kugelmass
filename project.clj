2d2fe208a905af07a616d37d5fd988b48c10d3af
saidone <saidone@saidone.org>
2021-01-05 08:54:43 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -7,22 +7,22 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [com.cognitect/transit-clj "1.0.324"]
-                 [ring "1.8.1"]
+                 [ring "1.8.2"]
                  [ring/ring-defaults "0.3.2"]
                  [bk/ring-gzip "0.3.0"]
                  [radicalzephyr/ring.middleware.logger "0.6.0"]
                  [clj-logging-config "1.9.12"]
-                 [environ "1.1.0"]
+                 [environ "1.2.0"]
                  [com.stuartsierra/component "1.0.0"]
                  [org.danielsz/system "0.4.5"]
-                 [org.clojure/tools.namespace "1.0.0"]
+                 [org.clojure/tools.namespace "1.1.0"]
                  [clj-time "0.15.2"]
-                 [compojure "1.6.1"]
+                 [compojure "1.6.2"]
                  [reagent "1.0.0"]
                  [clj-commons/secretary "1.2.4"]]
 
   :plugins [[lein-cljsbuild "1.1.8"]
-            [lein-environ "1.1.0"]]
+            [lein-environ "1.2.0"]]
 
   :min-lein-version "2.6.1"
 
