9f50033696ce078a3026035b2c5f5c9cf41931d1
saidone <saidone@saidone.org>
2021-04-28 18:57:30 +0200

    update ring

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.844" :scope "provided"]
                  [com.cognitect/transit-clj "1.0.324"]
-                 [ring "1.9.2"]
+                 [ring "1.9.3"]
                  [ring/ring-defaults "0.3.2"]
                  [bk/ring-gzip "0.3.0"]
                  [radicalzephyr/ring.middleware.logger "0.6.0"]
