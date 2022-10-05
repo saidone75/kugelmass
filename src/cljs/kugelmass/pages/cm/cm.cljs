f31a1bfc1574a43818a5e19fc16062efb6f2907d
saidone <saidone@saidone.org>
2022-10-05 08:26:10 +0200

    cm work in progress

--- src/cljs/kugelmass/pages/cm/cm.cljs
+++ src/cljs/kugelmass/pages/cm/cm.cljs
@@ -1,10 +1,25 @@
 ;; Copyright (c) 2020-2022 Saidone
 
-(ns kugelmass.pages.cm.cm)
+(ns kugelmass.pages.cm.cm
+  (:require [reagent.core :as r]))
 
-(def cm
-  [:div.page
-   ])
+(def page (r/atom {}))
+
+(defn display-file-content [content]
+  [:div (-> content .-target .-result)])
+
+(defn put-upload [e]
+  (let [reader (js/FileReader.)
+        file (-> e .-target .-files (aget 0))]
+    (set! (.-onload reader) #(swap! page assoc :content (display-file-content %)))
+    (.readAsText reader file)))
+
+(defn upload-button []
+  [:div
+   [:input
+    {:type "file" :accept ".xml" :on-change put-upload}]])
+
+(swap! page assoc :content (upload-button))
 
 (defn content []
-  (atom {:content cm}))
+  page)
