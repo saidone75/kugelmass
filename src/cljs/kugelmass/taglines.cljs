5d2bf5ae974e228835216b91d2902bd4795ad779
saidone <saidone@saidone.org>
2024-12-12 21:45:43 +0100

    add tagline

--- src/cljs/kugelmass/taglines.cljs
+++ src/cljs/kugelmass/taglines.cljs
@@ -12,6 +12,7 @@
                "Better built. Better hacked."
                "go play leapfrog with a unicorn"
                ;;"a likely place - and probably true"
+               "Jeder für sich und Gott gegen alle"
                "the only winning move is not to play"
                "now she knows why her cat stays away from me! DAMNIT!"
                "Now they know how many holes it takes to fill the Albert Hall"
