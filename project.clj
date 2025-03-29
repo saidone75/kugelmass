fbcd15c6148aa3b084e1c381c0caea1d1966b71f
saidone <saidone@saidone.org>
2025-03-29 12:43:06 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kugelmass "0.3.9"
+(defproject kugelmass "0.3.10"
   :description "Source code for my web page saidone.org"
   :url "http://saidone.org"
   :license {:name "MIT"
--- src/cljs/kugelmass/core.cljs
+++ src/cljs/kugelmass/core.cljs
@@ -11,7 +11,7 @@
             [goog.history.EventType :as EventType])
   (:import goog.History))
 
-(defn version [] "0.3.9")
+(defn version [] "0.3.10")
 
 (enable-console-print!)
 
