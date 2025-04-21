2ff8c0fc27c494b9867d4109abf2de5b0ff74957
saidone <saidone@saidone.org>
2025-04-21 18:45:26 +0200

    update taglines and quotes

--- src/cljs/kugelmass/taglines.cljs
+++ src/cljs/kugelmass/taglines.cljs
@@ -4,19 +4,12 @@
 
 (def taglines ["DON'T PANIC"
                "call me maybe"
-               "Shaken, not stirred"
-               "not a flotation device"
-               "Little. Green. Better."
                "the enemy knows the system"
-               ;;"tickling the dragon's tail"
-               "Better built. Better hacked."
-               "go play leapfrog with a unicorn"
-               ;;"a likely place - and probably true"
                "Jeder für sich und Gott gegen alle"
                "the only winning move is not to play"
+               "col Squarzia e Curzio nel sacro palazo"
                "now she knows why her cat stays away from me! DAMNIT!"
-               "Now they know how many holes it takes to fill the Albert Hall"
-               "Brezhnev took Afghanistan. Begin took Beirut. Galtieri took the Union Jack."])
+               "Now they know how many holes it takes to fill the Albert Hall"])
 
 (defn get-tagline []
   (rand-nth taglines))
